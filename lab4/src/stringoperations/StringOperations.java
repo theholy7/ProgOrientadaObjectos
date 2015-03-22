a224cc39ad120e77fba18f8eb4587570808dc05b
Jose Antunes <theholy7@gmail.com>
2015-03-22 11:05:28 +0000

    Initial Commit: Lab4

---
+++ lab4/src/stringoperations/StringOperations.java
@@ -0,0 +1,27 @@
+package stringoperations;
+
+public class StringOperations {
+	
+	public void insertString(){
+		
+	}
+	
+	public int removeString(){
+		return 0;
+	}
+	
+	public int length(){
+		return 0;
+	}
+	
+	public String toString(){
+		return null;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
